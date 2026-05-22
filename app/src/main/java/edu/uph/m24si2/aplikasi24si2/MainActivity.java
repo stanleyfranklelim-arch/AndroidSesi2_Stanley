eb4834a150230732b270e9f41dae3ea5935439d8
stanleyfranklelim <stanleyfranklelim@gmail.com>
2026-05-22 20:12:26 +0700

    Initial commit

---
+++ app/src/main/java/edu/uph/m24si2/aplikasi24si2/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.uph.m24si2.aplikasi24si2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
